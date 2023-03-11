c7d6e7856eb83fa44bec19edf7d7a068c6a038f5
JustLuchi <oluchiokoro942@gmail.com>
2023-03-11 07:20:25 -0800

    hi

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,16 +1,12 @@
-import importlib.util
-
+#!/usr/bin/python3
 def magic_calculation(a, b):
-    if a < b:
-        spec = importlib.util.spec_from_file_location("magic_calculation_102", "./magic_calculation_102.py")
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-        add = getattr(module, "add")
-        sub = getattr(module, "sub")
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
+    c = 0
+    for i in range(4, 6):
+        c = add(c, i)
+    return sub(a, b) if a < b else add(c, sub(a, b))
+
+if __name__ == '__main__':
+    from magic_calculation_102 import add, sub
+    print(magic_calculation(1, 2))
+    print(magic_calculation(4, 2))
 
