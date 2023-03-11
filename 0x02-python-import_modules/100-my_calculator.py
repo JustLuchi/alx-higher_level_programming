bbcf9fb6b66921679feb37d69bdf6c4ca974e2cc
JustLuchi <oluchiokoro942@gmail.com>
2023-03-11 07:41:41 -0800

    hi

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 import sys
+from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    operator = sys.argv[2]
+a = int(sys.argv[1])
+op = sys.argv[2]
+b = int(sys.argv[3])
 
-    if operator == "+":
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    elif operator == "-":
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    elif operator == "*":
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    elif operator == "/":
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+if op == '+':
+    result = add(a, b)
+elif op == '-':
+    result = sub(a, b)
+elif op == '*':
+    result = mul(a, b)
+elif op == '/':
+    result = div(a, b)
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
+
+print("{:d} {:s} {:d} = {:d}".format(a, op, b, result))
 
